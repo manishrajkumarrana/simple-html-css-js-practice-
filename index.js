73fa15efe865e5edfe44ca71ddaf31b2c56b080b
Manish Rana <119585211+manishrajkumarrana@users.noreply.github.com>
2022-12-01 16:32:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+const average = require("./mod")
+// const mod = require("./mod")
+console.log ( average([3,4]))
+console.log("this is index.js")
---
+++ mod.js
@@ -0,0 +1,10 @@
+console.log ( "this is mod")
+function average ( arr)
+{
+    arr.forEach(Element=>{
+    sum += Element ;
+
+    });
+    return sum/arr.lenght;
+}
+module.exports=average;
